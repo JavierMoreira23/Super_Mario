1a48d9dc2b1d1e9412e3e4a613e58ea2e3c32de9
Javier Moreira <javier10moreira@hotmail.com>
2025-05-23 15:37:33 -0500

    creando animacion de mario

--- game.js
+++ game.js
@@ -31,15 +31,31 @@ function preload() {
         'assets/entities/mario.png',
         { frameWidth: 18, frameHeight: 16 }
     )
+
+    this.anims.create({
+        key : 'mario-run',
+        frames: this.anims.generateFrameNumbers(
+            'mario', { start: 0, end: 4}),
+            repeat: -1,
+    })
 }
 
 function create() {
     this.add.image(100, 50, 'cloud1').setScale(0.15)
 
     this.add.tileSprite(0, config.height-32, config.width,32, 'floorbricks')
-    
     .setOrigin(0, 0)
-    this.add.sprite(50, 210, 'mario').setOrigin(0, 1)
+    
+    this.mario = this.add.sprite(50, 210, 'mario').setOrigin(0, 1)
+
+    this.keys = this.input.keyboard.createCursorKeys();
 }
 
-function update() {}
+function update() {
+    if (this.keys.left.isDown){
+        this.mario.x -=2
+
+    } else if (this.keys.right.isDown){
+        this.mario.x +=2
+    }
+}
