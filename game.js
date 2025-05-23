e8b8cc1d88ff9faeb635736b4fe9919bbb6b4671
Javier Moreira <javier10moreira@hotmail.com>
2025-05-23 16:51:19 -0500

    creando  camara

--- game.js
+++ game.js
@@ -45,11 +45,27 @@ function preload() {
 function create() {
     this.add.image(100, 50, 'cloud1').setScale(0.15)
 
-    this.add.tileSprite(0, config.height-32, config.width,32, 'floorbricks')
-    .setOrigin(0, 0)
+    this.floor = this.physics.add.staticGroup()
+    this.floor.create(0, config.height - 16, 'floorbricks')
+    .setOrigin(0, 0.5)
+    .refreshBody()
+
+    this.floor.create(150,config.height - 16, 'floorbricks')
+    .setOrigin(0, 0.5)
+    .refreshBody()
     
     //this.mario = this.add.sprite(50, 210, 'mario').setOrigin(0, 1)
-     this.mario = this.physics.add.sprite(50, 210, 'mario').setOrigin(0, 1).setGravityY(300)
+    this.mario = this.physics.add.sprite(50, 210, 'mario')
+    .setOrigin(0, 1)
+    .setCollideWorldBounds(true)
+    .setGravityY(420)
+    
+    this.physics.world.setBounds(0,0,2000, config.height)
+    this.physics.add.collider(this.mario, this.floor)
+
+    this.cameras.main.setBounds(0, 0, 2000, config.height)
+    this.cameras.main.startFollow(this.mario)       
+
     this.anims.create({
         key : 'mario-run',
         frames: this.anims.generateFrameNumbers(
@@ -85,8 +101,9 @@ function update() {
         this.mario.anims.play('mario-idle',true)
     }
 
-    if(this.keys.up.isDown){
+    if(this.keys.up.isDown && this.mario.body.touching.down){
+        this.mario.setVelocityY(-295)
         this.mario.anims.play('mario-jump',true)
-        this.mario.y -=5
+    
     }
 }
